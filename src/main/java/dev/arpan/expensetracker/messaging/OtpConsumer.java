e954aacbc035abc65b6d59fc853cd219c42840df
Arpan Duari <arpanduari26@gmail.com>
2025-08-14 00:57:25 +0530

    Integrate message queue: OTP sending implementation

---
+++ src/main/java/dev/arpan/expensetracker/messaging/OtpConsumer.java
@@ -0,0 +1,24 @@
+package dev.arpan.expensetracker.messaging;
+
+import dev.arpan.expensetracker.service.EmailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+/**
+ * @author arpan
+ * @since 8/13/25
+ */
+@Service
+@RequiredArgsConstructor
+public class OtpConsumer {
+    private final EmailService emailService;
+    @RabbitListener(queues = "${app.queue.otp.queue}")
+    public void receiveOtpMessage(Map<String, String> message){
+        String email = message.get("email");
+        String otp = message.get("otp");
+        emailService.sendOtp(email, otp);
+    }
+}
---
+++ src/main/java/dev/arpan/expensetracker/messaging/OtpProducer.java
@@ -0,0 +1,28 @@
+package dev.arpan.expensetracker.messaging;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.lang.reflect.MalformedParameterizedTypeException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author arpan
+ * @since 8/13/25
+ */
+@Service
+@RequiredArgsConstructor
+public class OtpProducer {
+    private final RabbitTemplate rabbitTemplate;
+    @Value("${app.queue.otp.queue}")
+    private String otpQueueName;
+    public void sendOtpMessage(String email, String otp){
+        Map<String, String> payLoad = new HashMap<>();
+        payLoad.put("email", email);
+        payLoad.put("otp", otp);
+        rabbitTemplate.convertAndSend(otpQueueName, payLoad);
+    }
+}
