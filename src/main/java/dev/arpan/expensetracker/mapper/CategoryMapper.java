46458ae0c3152e1e4e4b6f19d88dee0fd0751854
Arpan Duari <arpanduari26@gmail.com>
2025-09-04 00:22:51 +0530

    fix bug category default icon

--- src/main/java/dev/arpan/expensetracker/mapper/CategoryMapper.java
+++ src/main/java/dev/arpan/expensetracker/mapper/CategoryMapper.java
@@ -23,7 +23,8 @@ public final class CategoryMapper {
     public static Category toCategory(CategoryRequest categoryRequest) {
         return Category.builder()
                 .name(categoryRequest.getName())
-                .icon(categoryRequest.getIcon())
+                .icon(categoryRequest.getIcon() != null && !categoryRequest.getIcon().isBlank() ?
+                        categoryRequest.getIcon() : "\uD83D\uDCB8")
                 .build();
     }
 }
